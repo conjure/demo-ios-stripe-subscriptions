89e2b2831031768d3c5d3a69b1c0de2d204d4664
Dinesh Vijaykumar <dineshvijaykumar6@gmail.com>
2021-11-17 17:29:11 +0000

    Set up constants

--- Stripe-Subscription-Example/Constants.swift
+++ Stripe-Subscription-Example/Constants.swift
@@ -9,4 +9,6 @@ import Foundation
 
 struct Constants {
     static let stripePublishableKey = "ENTER_STRIPE_PUBLISHABLE_API_KEY"
+    static let backendUrl = "ENTER_BACKEND_URL"
+    static let priceID = "ENTER_STRIPE_PRICE_ID"
 }
