1d76dbf64ae784ee7548deacb7891d18f273da9c
Dinesh Vijaykumar <dineshvijaykumar6@gmail.com>
2021-11-17 15:56:57 +0000

    Set up stripe SDK with publishable key

--- Stripe-Subscription-Example/AppDelegate.swift
+++ Stripe-Subscription-Example/AppDelegate.swift
@@ -6,14 +6,15 @@
 //
 
 import UIKit
+import Stripe
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        StripeAPI.defaultPublishableKey = Constants.stripePublishableKey
+
         return true
     }
 
---
+++ Stripe-Subscription-Example/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Stripe-Subscription-Example
+//
+//  Created by Dinesh Vijaykumar on 17/11/2021.
+//
+
+import Foundation
+
+struct Constants {
+    static let stripePublishableKey = "ENTER_STRIPE_PUBLISHABLE_API_KEY"
+}
